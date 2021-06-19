472e8a33e87f198959257bae3fb72ffa289c9d81
oliverbgit <60161804+oliverbgit@users.noreply.github.com>
2021-06-19 00:21:44 -0400

    deployed 2.1 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 
 setup(name='dumda',
-      version='2.0',
+      version='2.1',
       description='generate highly customizable dummy data for data science testing',
       long_description=read('README.md'),
       long_description_content_type='text/markdown',
